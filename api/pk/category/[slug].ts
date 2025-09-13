2a60bc392a24de1d32161525cae158d4d415f506
General <mshumayl66@gmail.com>
2025-09-13 17:33:59 -0400

    test

--- api/pk/category/[slug].ts
+++ api/pk/category/[slug].ts
@@ -63,6 +63,7 @@ export default async function handler(req: any, res: any) {
         return res.status(200).json({ items: fresh.items })
       }
     }
+    //
     res.setHeader('X-Cache', 'MISS')
     // Use Webz-only providers
     const providers = getProvidersForPK()
